079346e1ddab0964b4d9a65bebad91c4ae9c1695
Cam Swords <cam.swords@gmail.com>
2015-11-28 19:43:41 +1100

    dont update, dont upgrade. Hopefully this will make it more likely for the script to install in the future (because packages wont have changed)

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# install the latest patches for installed software
-apt-get update
-apt-get upgrade --assume-yes
-
 # install git, install code
 apt-get install git --assume-yes
 git clone https://github.com/camswords/raspberry-pi-instagram-printer.git
