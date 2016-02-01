f2a7ce3a9368a436f3cb767f39c771333d2e1f7e
Cam Swords <cam.swords@gmail.com>
2016-02-01 21:05:31 +1100

    print the job ids

--- src/lib/jobs.py
+++ src/lib/jobs.py
@@ -6,6 +6,5 @@ class Jobs:
     def __init__(self, connection):
         self.connection = connection
 
-
     def __str__(self):
-        return "%s" % self.jobs
+        return "%s" % self.connection.getJobs().keys()
