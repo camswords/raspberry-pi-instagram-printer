4212e14fba9ba50c81444860d552cf3d52be49d9
Cam Swords <cam.swords@gmail.com>
2016-02-23 23:37:36 +1100

    fixed a small syntax error.

--- src/lib/power.py
+++ src/lib/power.py
@@ -4,11 +4,11 @@ import time
 class Power:
 
     def turn_off(self):
-        for x in range(0, 5)
+        for x in range(0, 5):
             check_call(["/home/pi/send", "001000001111101000000110"])
             time.sleep(1)
 
     def turn_on(self):
-        for x in range(0, 5)
+        for x in range(0, 5):
             check_call(["/home/pi/send", "001000001111101000001110"])
             time.sleep(1)
